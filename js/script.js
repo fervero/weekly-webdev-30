0ecc70dbc847e027bc58b5f902aef818897280ef
Maciej Bojko <maciej.bojko@gmail.com>
2017-05-09 19:34:15 +0200

    Cleaned everything up a little, rewrote the script without jQuery.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-$(function() {
-    var $widgetHide = $("#widget_hide"),
-        $widgetShow = $("#widget_show"),
-        $nav = $(".navbar");
-    $widgetHide.removeClass("nav__widget_hidden");
-    $widgetHide.on('click', function() {
-        $("nav").removeClass("nav_expanded").addClass("nav_collapsed");
+window.onload = function() {
+    var widgetHide = document.querySelector("#widget_hide"),
+        widgetShow = document.querySelector("#widget_show"),
+        nav = document.querySelector(".navbar");
+    
+    function addClass(el, className) {
+        if (el.classList)
+            el.classList.add(className);
+        else
+            el.className += ' ' + className;        
+    }    
+    function removeClass(el, className) {
+        if (el.classList)
+            el.classList.remove(className);
+        else
+            el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+    removeClass(widgetHide, "nav__widget_hidden");
+    widgetHide.addEventListener('click', function() {
+        removeClass(nav, "nav_expanded");
+        addClass(nav, "nav_collapsed");        
+    })
+    widgetShow.addEventListener('click', function() {
+        removeClass(nav, "nav_collapsed");
+        addClass(nav, "nav_expanded");
     });
-    $widgetShow.on('click', function() {
-        $("nav").removeClass("nav_collapsed").addClass("nav_expanded");
-    });
-
-});
+};
